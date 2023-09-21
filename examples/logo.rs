6a05d0cbb9f14f32a127ee8c76a95406bf3b295a
Felipe Coury <felipe.coury@gmail.com>
2023-09-21 15:22:15 -0300

    feat: completed example logo label

--- examples/logo.rs
+++ examples/logo.rs
@@ -1,10 +1,16 @@
-use zpl::{BarcodeType, LabelBuilder};
+use zpl::{BarcodeType, FontName, LabelBuilder};
 
 fn main() {
-    let label = LabelBuilder::new()
-        .add_text(10, 10, "Hello World")
-        .add_barcode(10, 30, BarcodeType::Code128, "12345")
-        .add_image(10, 50, "examples/assets/logo.jpg")
+    let label = LabelBuilder::new(50, 25, None)
+        .font_name(FontName::Zebra0)
+        .font_size(20)
+        .add_text(10, 10, "Last, First")
+        .add_text(320, 10, "T1 - 20'")
+        .add_text(10, 30, "12/12/1962")
+        .add_barcode(120, 80, BarcodeType::Code128, "12345")
+        .add_image(0, 140, "examples/assets/logo.jpg")
+        .font_size(10)
+        .add_text(300, 180, "2023-01-01 10:05pm")
         .build();
-    println!("^XA{}^XZ", label.to_zpl());
+    println!("{}", label.to_zpl());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug, Clone)]
 pub struct Label {
     elements: Vec<Element>,
@@ -12,25 +14,43 @@ impl Label {
             zpl.push_str(&element.to_zpl());
         }
 
-        zpl
+        format!("^XA{}^XZ", zpl)
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Element {
-    typ: ElementType,
-    x: u32,
-    y: u32,
+pub enum Element {
+    Component { typ: ComponentType, x: u32, y: u32 },
+    Setting(SettingType),
 }
 
-impl Element {
+#[derive(Debug, Clone)]
+pub enum SettingType {
+    LabelDimensions(u32, u32, Option<u32>),
+    FontSize(i32),
+    FontName(FontName),
+}
+
+impl SettingType {
     pub fn to_zpl(&self) -> String {
-        self.typ.to_zpl(self.x, self.y)
+        match self {
+            SettingType::LabelDimensions(_, height, dpmm) => {
+                let dpmm = match dpmm {
+                    Some(dpmm) => dpmm.to_string(),
+                    None => "8".to_string(),
+                };
+                format!("^XA^LL{height}\n^JM{dpmm}^LH0,0^BY,,40\n",)
+            }
+            SettingType::FontSize(size) => format!("^CF,{size},\n"),
+            SettingType::FontName(font_name) => match font_name {
+                FontName::Zebra0 => format!("^CF{font_name},,\n"),
+            },
+        }
     }
 }
 
 #[derive(Debug, Clone)]
-pub enum ElementType {
+pub enum ComponentType {
     Image(String),
     Text(String),
     Barcode {
@@ -45,47 +65,50 @@ pub enum ElementType {
     },
 }
 
-impl ElementType {
-    pub fn to_zpl(&self, x: u32, y: u32) -> String {
+impl Element {
+    pub fn to_zpl(&self) -> String {
         match self {
-            ElementType::Image(path) => {
-                format!("^FO{},{}{}^FS\n", x, y, image_to_zpl(path))
-            }
-            ElementType::Text(text) => format!("^FO{},{}^FD{}^FS\n", x, y, text),
-            ElementType::Barcode { typ, data } => format!(
-                "^FO{},{}^BY2^B{}^FD{}^FS\n",
-                x,
-                y,
-                match typ {
-                    BarcodeType::Code39 => "3",
-                    BarcodeType::Code128 => "C",
-                    BarcodeType::Aztec => "Z",
-                    BarcodeType::DataMatrix => "D",
-                    BarcodeType::EAN13 => "E",
-                    BarcodeType::EAN8 => "E",
-                    BarcodeType::GS1DataBar => "K",
-                    BarcodeType::QRCode => "Q",
-                },
-                data
-            ),
-            ElementType::Icon(_typ) => {
-                format!("^FO{},{}^GRI,,Y,N^FS\n^FO{},{}^GRI,,Y,N^FS\n", x, y, x, y,)
-            }
-            ElementType::Shape {
-                typ,
-                width,
-                height: _height,
-            } => format!(
-                "^FO{},{}^GB{},{}^FS\n",
-                x,
-                y,
-                match typ {
-                    ShapeType::Ellipse => "E",
-                    ShapeType::Rectangle => "R",
-                    ShapeType::Triangle => "T",
-                },
-                width
-            ),
+            Element::Setting(typ) => typ.to_zpl(),
+            Element::Component { typ, x, y } => match typ {
+                ComponentType::Image(path) => {
+                    format!("^FO{},{}{}^FS\n", x, y, image_to_zpl(path))
+                }
+                ComponentType::Text(text) => format!("^FO{},{}^FD{}^FS\n", x, y, text),
+                ComponentType::Barcode { typ, data } => format!(
+                    "^FO{},{}^BY2^B{}^FD{}^FS\n",
+                    x,
+                    y,
+                    match typ {
+                        BarcodeType::Code39 => "3",
+                        BarcodeType::Code128 => "C",
+                        BarcodeType::Aztec => "Z",
+                        BarcodeType::DataMatrix => "D",
+                        BarcodeType::EAN13 => "E",
+                        BarcodeType::EAN8 => "E",
+                        BarcodeType::GS1DataBar => "K",
+                        BarcodeType::QRCode => "Q",
+                    },
+                    data
+                ),
+                ComponentType::Icon(_typ) => {
+                    format!("^FO{},{}^GRI,,Y,N^FS\n^FO{},{}^GRI,,Y,N^FS\n", x, y, x, y,)
+                }
+                ComponentType::Shape {
+                    typ,
+                    width,
+                    height: _height,
+                } => format!(
+                    "^FO{},{}^GB{},{}^FS\n",
+                    x,
+                    y,
+                    match typ {
+                        ShapeType::Ellipse => "E",
+                        ShapeType::Rectangle => "R",
+                        ShapeType::Triangle => "T",
+                    },
+                    width
+                ),
+            },
         }
     }
 }
@@ -119,26 +142,37 @@ pub enum ShapeType {
     Triangle,
 }
 
+#[derive(Debug, Clone)]
+pub enum FontName {
+    Zebra0,
+}
+
+impl Display for FontName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FontName::Zebra0 => write!(f, "0"),
+        }
+    }
+}
+
 pub struct LabelBuilder {
     elements: Vec<Element>,
 }
 
-impl Default for LabelBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl LabelBuilder {
-    pub fn new() -> Self {
+    pub fn new(width_in_mm: u32, height_in_mm: u32, dpmm: Option<u32>) -> Self {
         LabelBuilder {
-            elements: Vec::new(),
+            elements: vec![Element::Setting(SettingType::LabelDimensions(
+                width_in_mm,
+                height_in_mm,
+                dpmm,
+            ))],
         }
     }
 
     pub fn add_image(mut self, x: u32, y: u32, path: &str) -> Self {
-        self.elements.push(Element {
-            typ: ElementType::Image(path.to_string()),
+        self.elements.push(Element::Component {
+            typ: ComponentType::Image(path.to_string()),
             x,
             y,
         });
@@ -146,8 +180,8 @@ impl LabelBuilder {
     }
 
     pub fn add_text(mut self, x: u32, y: u32, text: &str) -> Self {
-        self.elements.push(Element {
-            typ: ElementType::Text(text.to_string()),
+        self.elements.push(Element::Component {
+            typ: ComponentType::Text(text.to_string()),
             x,
             y,
         });
@@ -155,8 +189,8 @@ impl LabelBuilder {
     }
 
     pub fn add_barcode(mut self, x: u32, y: u32, barcode_type: BarcodeType, data: &str) -> Self {
-        self.elements.push(Element {
-            typ: ElementType::Barcode {
+        self.elements.push(Element::Component {
+            typ: ComponentType::Barcode {
                 typ: barcode_type,
                 data: data.to_string(),
             },
@@ -167,8 +201,8 @@ impl LabelBuilder {
     }
 
     pub fn add_icon(mut self, x: u32, y: u32, icon_type: IconType) -> Self {
-        self.elements.push(Element {
-            typ: ElementType::Icon(icon_type),
+        self.elements.push(Element::Component {
+            typ: ComponentType::Icon(icon_type),
             x,
             y,
         });
@@ -183,8 +217,8 @@ impl LabelBuilder {
         width: u32,
         height: u32,
     ) -> Self {
-        self.elements.push(Element {
-            typ: ElementType::Shape {
+        self.elements.push(Element::Component {
+            typ: ComponentType::Shape {
                 typ: shape_type,
                 width,
                 height,
@@ -192,6 +226,18 @@ impl LabelBuilder {
             x,
             y,
         });
+        self
+    }
+
+    pub fn font_size(mut self, size: i32) -> Self {
+        self.elements
+            .push(Element::Setting(SettingType::FontSize(size)));
+        self
+    }
+
+    pub fn font_name(mut self, font_name: FontName) -> Self {
+        self.elements
+            .push(Element::Setting(SettingType::FontName(font_name)));
         self
     }
 
